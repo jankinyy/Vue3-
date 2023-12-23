887b52178b0426305209386d70f718862feeb48c
wuyangyang <15203977881@163.com>
2023-12-24 04:19:34 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="toggle">显示隐藏图片</button>
+  <img v-show="show" alt="Vue logo" src="./assets/logo.png" />
+  <hr />
+  计数器：{{ count }} <button @click="increment">累加</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+      count: 0,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script>
